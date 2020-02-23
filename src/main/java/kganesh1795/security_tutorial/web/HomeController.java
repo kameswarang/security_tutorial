d9a5581d2c162dd35cbde6cdbb2bf6dd4f344e04
Kameswaran Ganesh <kameswaran1795@gmail.com>
2020-02-23 14:05:32 +0000

    Updated homepage with nav

--- src/main/java/kganesh1795/security_tutorial/web/HomeController.java
+++ src/main/java/kganesh1795/security_tutorial/web/HomeController.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping(path={"/home", "/"})
 public class HomeController {
     @GetMapping
     public String getHome(Model model, @AuthenticationPrincipal(errorOnInvalidType=true) UserDetails user) {
-        //System.out.println(user.getUsername());
         model.addAttribute("user", user);
         return "home";
     }
